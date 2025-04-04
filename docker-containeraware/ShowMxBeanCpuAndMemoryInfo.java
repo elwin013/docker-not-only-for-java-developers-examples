0adfc2bb206c700c7dac1e6c8e816ca8b877b131
Kamil Banach <kontakt@elwin013.com>
2025-04-04 22:10:00 +0200

    Update example for containeraware
    * Extend makefile - add `-Xlog:gc+init` to see more details
    * Show free / total memory in MiB also

--- docker-containeraware/ShowMxBeanCpuAndMemoryInfo.java
+++ docker-containeraware/ShowMxBeanCpuAndMemoryInfo.java
@@ -6,8 +6,8 @@ public class ShowMxBeanCpuAndMemoryInfo {
         var osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
         System.out.printf("Runtime.availableProcessors: %d%n", Runtime.getRuntime().availableProcessors());
         System.out.printf("OperatingSystemMXBean.getAvailableProcessors: %d%n", osBean.getAvailableProcessors());
-        System.out.printf("OperatingSystemMXBean.getFreeMemorySize: %d%n", osBean.getFreeMemorySize());
-        System.out.printf("OperatingSystemMXBean.getTotalPhysicalMemorySize: %d%n", osBean.getTotalMemorySize());
+        System.out.printf("OperatingSystemMXBean.getFreeMemorySize: %d (%.2fMiB)%n", osBean.getFreeMemorySize(), osBean.getFreeMemorySize() / (1024.0 * 1024.0));
+        System.out.printf("OperatingSystemMXBean.getTotalPhysicalMemorySize: %d (%.2fMiB)%n", osBean.getTotalMemorySize(), osBean.getTotalMemorySize() / (1024.0 * 1024.0));
         System.out.printf("OperatingSystemMXBean.getFreeSwapSpaceSize: %d%n", osBean.getFreeSwapSpaceSize());
         System.out.printf("OperatingSystemMXBean.getTotalSwapSpaceSize: %d%n", osBean.getTotalSwapSpaceSize());
         System.out.printf("OperatingSystemMXBean.getCpuLoad: %f%n", osBean.getCpuLoad());
